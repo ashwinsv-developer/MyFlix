53ebbbac3fe1159abbec1296f6753e9467319421
Ashwin kumar <ashwinsv02@gmail.com>
2026-05-15 19:34:08 +0530

    Theme change implementation

--- app/src/main/java/com/example/myflix/MainActivity.kt
+++ app/src/main/java/com/example/myflix/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myflix.ui.theme.MyFlixTheme
@@ -18,11 +24,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyFlixTheme {
+            val systemTheme = isSystemInDarkTheme()
+            var isDarkMode by remember { mutableStateOf(systemTheme) }
+
+            MyFlixTheme(darkTheme = isDarkMode) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .clickable { isDarkMode = !isDarkMode }
                     )
                 }
             }
